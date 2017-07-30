6208a476d7fda11ba770a6426f5cefad1d899ad8
kuro-channel <takumikuroiwa@gmail.com>
2017-07-30 23:08:09 +0900

    2017/07/30 作業分
    Java本格入門実践中

---
+++ src/java_practice/Main.java
@@ -0,0 +1,54 @@
+package java_practice;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// メッセージの表示
+		System.out.println("Hello Java World!");
+		/* 複数コメント記載
+		 * 行を跨いで書くことが出来ます
+		 */
+
+		// p.38 if
+		int second = LocalDateTime.now().getSecond();
+		if(second % 2 ==0){
+			System.out.println(second + "は偶数です");
+		}else{
+			System.out.println(second + "は奇数です");
+		}
+		// p.39 if ~ / if else ~
+		int month = LocalDate.now().getMonthValue();
+
+		if(3<= month && month <=5){
+			System.out.println(month+ "月は春です");
+		}else if(6<= month && month <=8){
+			System.out.println(month+ "月は夏です");
+		}else if(9<= month && month <=11){
+			System.out.println(month+ "月は秋です");
+		}else{
+			System.out.println(month+ "月は冬です");
+		}
+
+		// p.40 switch
+		Month month2 = LocalDateTime.now().getMonth();
+
+		switch (month2) {
+		case JANUARY:
+
+
+			break;
+
+		default:
+			break;
+		}
+		// プリミティブ型と参照型
+		// 桁数
+		long amount = 123_456_789L;
+		System.out.println(amount);
+	}
+
+}
